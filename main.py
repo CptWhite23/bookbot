995a8d47a38d262b2d44c66f0a224603bb1f0f06
CptWhite23 <noeldand@gmail.com>
2024-12-21 20:14:57 +0100

    function for creating a report implemented

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
 
-    print(count_words(file_content))
-    print(count_chars(file_content))
+    print_report(file_content)
 
 def count_words(text):
     words = text.split()
@@ -20,5 +19,17 @@ def count_chars(text):
             char_dict[char_lowercase] = 1
     
     return char_dict
+
+def print_report(text):
+    words = count_words(text)
+    character_dict = count_chars(text)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words found in the document")
+
+    for key in character_dict:
+        print(f"The '{key}' character was found {character_dict[key]} times")
+
+    print("--- End report ---")
 
 main()
