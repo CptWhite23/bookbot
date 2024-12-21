89badf370767de77f2ed83efc039a239fe0e61ec
CptWhite23 <noeldand@gmail.com>
2024-12-21 20:09:59 +0100

    function for counting characters implemented

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ def main():
         file_content = f.read()
 
     print(count_words(file_content))
+    print(count_chars(file_content))
 
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(text):
+    char_dict = {}
+
+    for char in text:
+        char_lowercase = char.lower()
+        if char_lowercase in char_dict:
+            char_dict[char_lowercase] += 1
+        else:
+            char_dict[char_lowercase] = 1
+    
+    return char_dict
 
 main()
